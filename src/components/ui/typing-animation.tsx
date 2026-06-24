9e109467279b1d0fe10e511a4fc478b9504410c4
_Dal_ <daittwone@gmail.com>
2026-06-24 12:20:46 +0700

    refactor: reorganize project structure

--- src/components/ui/typing-animation.tsx
+++ src/components/ui/typing-animation.tsx
@@ -17,7 +17,7 @@ import {
   type MotionProps,
 } from "motion/react"
 
-import { cn } from "@/src/lib/utils"
+import { cn } from "@/lib/utils"
 
 const motionElements = {
   article: motion.article,
